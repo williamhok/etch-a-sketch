15205d2e3b2b4c46d2e9bdf5e5c30c7c120be3b2
william <173856982+williamhok@users.noreply.github.com>
2024-07-22 16:34:34 +1000

    done

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const container = document.querySelector(".container");
+const changeSize = document.querySelector(".change-size")
 
-const size = prompt("Enter size number (Max: 100)")
-
-for (let i = 0; i < size * size; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cells");
+function board(size) {
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cells");
+        
     
-
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "blue"
-    })
-
-    container.appendChild(cell);
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "blue"
+        })
+    
+        container.appendChild(cell);
+    }
 }
+
+board(16)
+
+changeSize.addEventListener("click", () => {
+    container.innerHTML = ""
+    let size = prompt("Enter size number (Max: 100)")
+
+    while (isNaN(size) || size > 100 || size < 1) {
+        alert("Error: valid input is between 1 and 100")
+        size = prompt("Enter size number (Max: 100)")
+    }
+
+    board(size)
+})

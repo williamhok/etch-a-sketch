6afda7fa16ce800cf4abbd8adf309d6702d0b401
william <173856982+williamhok@users.noreply.github.com>
2024-07-22 16:17:58 +1000

    Allow user to input size number

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 
 const size = prompt("Enter size number (Max: 100)")
 
-for (let i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cells");
     

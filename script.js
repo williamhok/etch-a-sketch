2b54b4c93dd41bbc217a88c23bd6d0dd2fc98d2c
william <201534572+williamhok@users.noreply.github.com>
2025-03-13 15:13:51 +1100

    Added function to create the board

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const container = document.querySelector(".container")
 const board = document.querySelector(".board")
+let size = 16
 
-for (i = 0; i < 16; i++) {
-    let divs = document.createElement("div")
-    divs.className = "square"
-    board.appendChild(divs)
+function createBoard(size) {
+    board.innerHTML = ""
+    for (i = 0; i < size * size; i++) {
+        let divs = document.createElement("div")
+        divs.className = "square"
+        board.appendChild(divs)
+    }
+
+    const squares = document.querySelectorAll(".square")
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "blue"
+        })
+    })
 }
 
-const squares = document.querySelectorAll(".square")
-squares.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.setAttribute("style", "background-color: blue")
-    })
+createBoard(size)
+
+const changeSize = document.querySelector(".change-size")
+
+
+changeSize.addEventListener("click", () => {
+    size = prompt("Enter size number (max: 100)")
+    createBoard(size)
 })
